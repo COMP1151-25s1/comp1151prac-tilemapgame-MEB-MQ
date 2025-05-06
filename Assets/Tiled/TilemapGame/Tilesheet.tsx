46da3596669d8b83e4bf2c140857d72560effe91
Mebbu <madison.berg@mq.edu.au>
2025-05-06 12:53:39 +1000

    did more chnges

---
+++ Assets/Tiled/TilemapGame/Tilesheet.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TinyDungeonSheet" tilewidth="16" tileheight="16" spacing="1" tilecount="132" columns="12">
+ <image source="../kenney_tiny-dungeon/Tilemap/tilemap.png" width="203" height="186"/>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="113">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="1.84375" width="9.96875" height="13.1875"/>
+  </objectgroup>
+ </tile>
+ <tile id="121">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="16" height="15"/>
+  </objectgroup>
+ </tile>
+</tileset>
