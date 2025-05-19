7931cbf4b3931cf1522aa49ff1a41bb74815a34a
Mebbu <madison.berg@mq.edu.au>
2025-05-19 18:45:22 +1000

    went through prac 11 stuff

--- Assets/Tiled/TilemapGame/Tilesheet.tsx
+++ Assets/Tiled/TilemapGame/Tilesheet.tsx
@@ -6,6 +6,16 @@
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="66">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="14" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="110">
+  <properties>
+   <property name="unity:pivot" value="X: 0.5, Y: 0.5"/>
+  </properties>
+ </tile>
  <tile id="113">
   <objectgroup draworder="index" id="2">
    <object id="1" x="3" y="1.84375" width="9.96875" height="13.1875"/>
@@ -15,5 +25,10 @@
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="1" width="16" height="15"/>
   </objectgroup>
+ </tile>
+ <tile id="122">
+  <properties>
+   <property name="unity:Pivot" value="X: 0.5, Y: 0.5"/>
+  </properties>
  </tile>
 </tileset>
